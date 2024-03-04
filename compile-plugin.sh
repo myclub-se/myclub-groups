86df149beb22aa90d2a56347c663b02e7af01ec8
Andréas Kühne <andreas@kuhne.se>
2024-03-04 19:29:14 +0100

    Fixed typo

--- compile-plugin.sh
+++ compile-plugin.sh
@@ -13,7 +13,7 @@ npm run build
 
 # Extract and rename translations
 wp i18n make-json languages --no-purge
-python3 tools/updated_translation_files.py
+python3 tools/update_translation_files.py
 
 # Create a zip file for the plugin
 zip -r myclub-groups.zip . -x ".idea/*" -x "*.git*" -x "node_modules/*" -x "blocks/src/*" -x "package.json" -x "package-lock.json" -x "composer.json" -x "composer.lock" -x "build.sh" -x "compile-plugin.sh"
