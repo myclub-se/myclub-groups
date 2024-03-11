c2f1d754e4bccb8c2c8822f5455c878129e9fdcb
Andréas Kühne <andreas@kuhne.se>
2024-03-11 09:17:58 +0100

    Updated handling of calendar translations.

--- blocks/src/calendar/edit.js
+++ blocks/src/calendar/edit.js
@@ -5,7 +5,10 @@ import './editor.scss';
 import {__} from "@wordpress/i18n";
 
 import {getMyClubGroups} from "../shared/edit-functions";
-import MyClubFullcalendar from "./myclub-fullcalendar";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import FullCalendar from "@fullcalendar/react";
 
 
 /**
@@ -32,13 +35,110 @@ export default function Edit( { attributes, setAttributes } ) {
 		value: ''
 	};
 
-	let myClubCalendarRef = useRef();
 	const options = {
-		calendarRef: myClubCalendarRef,
-		events: postEvents.events,
+		allDaySlot: false,
+		buttonText: {
+			today: __( 'today', 'myclub-groups' ),
+			month: __( 'month', 'myclub-groups' ),
+			week: __( 'week', 'myclub-groups' ),
+			day: __( 'day', 'myclub-groups' ),
+			list: __( 'list', 'myclub-groups' )
+		},
+		plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+		initialView: 'dayGridMonth',
+		headerToolbar: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'dayGridMonth,timeGridWeek,listMonth'
+		},
+		events: postEvents,
 		locale: currentLocale ? currentLocale.substring(0, 2) : 'sv',
-		firstDay: startOfWeek
+		firstDay: startOfWeek,
+		eventClick: (arg) => {
+			const item = arg.event;
+
+			const modal = outerRef.current.getElementsByClassName('calendar-modal')[0];
+			const content = modal.getElementsByClassName('modal-body')[0];
+			const close = modal.getElementsByClassName('close')[0];
+
+			let output = '<div class="name">' + item.extendedProps.type + '</div>';
+			output += '<table>';
+
+			output += `<tr><th>${ __( 'Calendar', 'myclub-groups' ) }</th><td>${item.extendedProps.calendar_name}</td></tr>`;
+			output += `<tr><th>${ __( 'Name', 'myclub-groups' ) }</th><td>${item.title}</td></tr>`;
+			output += `<tr><th>${ __( 'When', 'myclub-groups' ) }</th><td>${item.extendedProps.startTime.substring(0, 5)} - ${item.extendedProps.endTime.substring(0, 5)}</td></tr>`;
+			output += `<tr><th>${ __( 'Location', 'myclub-groups' ) }</th><td>${item.extendedProps.location}</td></tr>`;
+			if ( item.extendedProps.meetUpTime && item.extendedProps.meetUpTime !== item.extendedProps.startTime ) {
+				output += `<tr><th>${ __( 'Gathering time', 'myclub-groups' ) }</th><td>${item.extendedProps.meetUpTime.substring(0, 5)}</td></tr>`;
+			}
+			if ( item.extendedProps.meetUpPlace ) {
+				output += `<tr><th>${ __( 'Gathering location', 'myclub-groups' ) }</th><td>${item.extendedProps.meetUpPlace}</td></tr>`;
+			}
+			if ( item.extendedProps.description ) {
+				output += `<tr><th>${ __( 'Information', 'myclub-groups' ) }</th><td>${item.extendedProps.description}</td></tr>`;
+			}
+			output += '</table>'
+
+			content.innerHTML = output;
+
+			modal.classList.add( 'modal-open' );
+			modal.addEventListener( 'click', closeButtonListener );
+			close.addEventListener( 'click', closeButtonListener );
+		},
+		eventContent: (arg) => {
+			const item = arg.event;
+			const element = document.createElement('div');
+			let timeText = arg.timeText;
+			element.classList.add('fc-event-title');
+			element.classList.add( getColorClass ( item.extendedProps.base_type ) );
+
+			if ( item.extendedProps.meetUpTime && item.extendedProps.meetUpTime !== item.extendedProps.startTime ) {
+				if ( !timeText ) {
+					timeText = item.extendedProps.startTime.substring(0, 5);
+				}
+
+				timeText += ` (${item.extendedProps.meetUpTime.substring(0, 5)})`;
+			}
+
+			element.innerHTML = '<div class="myclub-groups-event-time">' +
+				timeText + '</div><div class="myclub-groups-event-title">' +
+				item.title +
+				'</div>';
+
+			let arrayOfDomNodes = [
+				element
+			];
+
+			return { domNodes: arrayOfDomNodes };
+		},
+		timeZone: 'Europe/Stockholm',
+		weekNumbers: true,
 	}
+	let calendarRef = useRef();
+	let outerRef = useRef();
+
+	const closeButtonListener = () => {
+		const modal = outerRef.current.getElementsByClassName('modal-open')[0];
+		const close = modal.getElementsByClassName('close')[0];
+
+		modal.classList.remove('modal-open');
+		close.removeEventListener('click', closeButtonListener);
+		modal.removeEventListener( 'click', closeButtonListener );
+	}
+
+	const getColorClass = ( baseType ) => {
+		switch ( baseType ) {
+			case 'match':
+				return 'red';
+			case 'training':
+				return 'green';
+			case 'meeting':
+				return 'blue';
+			default:
+				return 'yellow';
+		}
+	}
+
 
 	const subtractMinutes = (time, minutes) => {
 		let parts = time.split(':');
@@ -61,8 +161,8 @@ export default function Edit( { attributes, setAttributes } ) {
 	}, []);
 
 	useEffect(() => {
-		if (myClubCalendarRef && myClubCalendarRef.current) {
-			const api = myClubCalendarRef.current.getApi();
+		if (calendarRef && calendarRef.current) {
+			const api = calendarRef.current.getApi();
 			api.removeAllEvents();
 			api.addEventSource(postEvents.events);
 		}
@@ -151,8 +251,15 @@ export default function Edit( { attributes, setAttributes } ) {
 				</PanelBody>
 			</InspectorControls>
 			<div {...useBlockProps()}>
-				<div className="myclub-groups-calendar">
-					<MyClubFullcalendar {...options} />
+				<div className="myclub-groups-calendar" ref={ outerRef }>
+					<FullCalendar ref={ calendarRef } { ...options } />
+					<div className="calendar-modal">
+						<div className="modal-content">
+							<span className="close">&times;</span>
+							<div className="modal-body">
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
